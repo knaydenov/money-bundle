4aa2001d9cb333af2accd86f9dd857cc12cc0346
Klim Naydenov <knaydenov@gmail.com>
2018-12-10 23:10:02 +0300

    Add formatter

--- Tests/App/src/Controller/DefaultController.php
+++ Tests/App/src/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 namespace Kna\MoneyBundle\Tests\App\Controller;
 
 
+use Money\Currency;
+use Money\Formatter\AggregateMoneyFormatter;
+use Money\Money;
 use Money\Parser\AggregateMoneyParser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,15 +17,25 @@ class DefaultController extends AbstractController
      */
     protected $parser;
 
-    public function __construct(AggregateMoneyParser $parser)
+    /**
+     * @var AggregateMoneyFormatter
+     */
+    protected $formatter;
+
+    public function __construct(
+        AggregateMoneyParser $parser,
+        AggregateMoneyFormatter $formatter
+    )
     {
         $this->parser = $parser;
+        $this->formatter = $formatter;
     }
 
     public function index(): Response
     {
         $data = [
-            'money' => $this->parser->parse('$100')
+            'money' => $this->parser->parse('$100'),
+            'string' => $this->formatter->format(new Money(10000, new Currency('USD')))
         ];
         return new JsonResponse($data);
     }
